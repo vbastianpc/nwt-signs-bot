d5121b7ffe4ebeb3133a2ca6ade0f7802d71a3fe
vbastianpc <vbastianpalavecino@gmail.com>
2021-04-22 22:36:02 -0400

    List of custom handlers inside module

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -5,3 +5,23 @@ from handlers.misc import (botfather_handler, test_handler, info_inline_handler,
                            text_fallback_handler, all_fallback_handler)
 from handlers.auth import (start_handler, auth_handler, permiso_handler,
                            delete_user_handler, getting_user_handler, helper_admin_handler)
+
+# Order matters
+handlers = [
+    parse_handler,
+    inline_handler,
+    inline_fallback_handler,
+    start_handler,
+    test_handler,
+    lang_handler,
+    quality_handler,
+    auth_handler,
+    permiso_handler,
+    info_inline_handler,
+    delete_user_handler,
+    botfather_handler,
+    getting_user_handler,
+    helper_admin_handler,
+    text_fallback_handler,
+    all_fallback_handler,
+]
--- bot/main.py
+++ bot/main.py
@@ -5,24 +5,7 @@ import logging
 from telegram import Update
 from telegram.ext import Updater, CallbackContext
 
-from handlers import (
-    parse_handler,
-    inline_handler,
-    inline_fallback_handler,
-    start_handler,
-    test_handler,
-    lang_handler,
-    quality_handler,
-    auth_handler,
-    permiso_handler,
-    info_inline_handler,
-    delete_user_handler,
-    botfather_handler,
-    getting_user_handler,
-    helper_admin_handler,
-    text_fallback_handler,
-    all_fallback_handler,
-)
+from handlers import handlers
 from secret import TOKEN
 
 
@@ -41,24 +24,6 @@ def log_error(_: Update, context: CallbackContext) -> None:
 def main() -> None:
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
-    handlers = [
-        parse_handler,
-        inline_handler,
-        inline_fallback_handler,
-        start_handler,
-        test_handler,
-        lang_handler,
-        quality_handler,
-        auth_handler,
-        permiso_handler,
-        info_inline_handler,
-        delete_user_handler,
-        botfather_handler,
-        getting_user_handler,
-        helper_admin_handler,
-        text_fallback_handler,
-        all_fallback_handler,
-    ]
     for handler in handlers:
         dispatcher.add_handler(handler)
     dispatcher.add_error_handler(log_error)
