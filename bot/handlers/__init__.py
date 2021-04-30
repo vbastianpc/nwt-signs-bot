38629f1f07a688ba7e14d15486aa67d051a0de85
vbastianpc <vbastianpalavecino@gmail.com>
2021-04-30 17:36:58 -0400

    relative imports

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,10 +1,10 @@
-from handlers.bible import parse_handler
-from handlers.settings import lang_handler, quality_handler
-from handlers.inline_bible import inline_handler
-from handlers.misc import (botfather_handler, test_handler, info_inline_handler,
-                           text_fallback_handler, all_fallback_handler)
-from handlers.auth import (start_handler, auth_handler, permiso_handler,
-                           delete_user_handler, getting_user_handler, helper_admin_handler)
+from .bible import parse_handler
+from .settings import lang_handler, quality_handler
+from .inline_bible import inline_handler
+from .misc import (botfather_handler, test_handler, info_inline_handler,
+                   text_fallback_handler, all_fallback_handler)
+from .auth import (start_handler, auth_handler, permiso_handler,
+                   delete_user_handler, getting_user_handler, helper_admin_handler)
 
 # Order matters
 handlers = [
