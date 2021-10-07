b03b7a6fcb21b349ca56c9eb06bf9de271bb3aaa
vbastianpc <vbastianpalavecino@gmail.com>
2021-10-06 23:51:13 -0300

    verify correct env varriables

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,20 @@
 import os
+from typing import Type
 
 TOKEN = os.getenv('TOKEN_NWT')
-ADMIN = os.getenv('USER_ID_ADMIN')
-CHANNEL_ID = os.getenv('CHANNEL_ID')
+if TOKEN is None:
+    raise Exception('You must define the environment variable TOKEN_NWT')
+
+try:
+    ADMIN = int(os.getenv('USER_ID_ADMIN'))
+except TypeError:
+    raise Exception('You must define the environment variable USER_ID_ADMIN')
+except ValueError:
+    raise Exception('Environment variable ADMIN must be a integer')
+
+try:
+    CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+except TypeError:
+    raise Exception('You must define the environment variable CHANNEL_ID')
+except ValueError:
+    raise Exception('Environment variable CHANNEL_ID must be a integer')
