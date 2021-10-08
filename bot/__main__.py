328c32efe324e0e0585b786ef5517bbc6b79fe04
vbastianpc <vbastianpalavecino@gmail.com>
2021-10-07 21:10:46 -0300

    remove log_error

--- bot/__main__.py
+++ bot/__main__.py
@@ -7,8 +7,9 @@ import logging
 from telegram import Update
 from telegram.ext import Updater, CallbackContext
 
-from bot.handlers import handlers
 from bot import TOKEN
+from bot.handlers import handlers
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s',
@@ -18,15 +19,11 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def log_error(_: Update, context: CallbackContext) -> None:
-    logger.error(context.error)
-
 
 if __name__ == '__main__':
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
     for handler in handlers:
         dispatcher.add_handler(handler)
-    # dispatcher.add_error_handler(log_error)
     updater.start_polling()
     updater.idle()
