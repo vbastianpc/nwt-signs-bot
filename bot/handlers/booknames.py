ebcc18dfdb0070ea802e4e93bbb37ad9bcf96dac
vbastianpc <vbastianpalavecino@gmail.com>
2021-11-26 16:56:44 -0300

    remove unused lines

--- bot/handlers/booknames.py
+++ bot/handlers/booknames.py
@@ -3,30 +3,21 @@ import logging
 import json
 
 from unidecode import unidecode
-from telegram import ChatAction
-from telegram import InlineKeyboardButton
-from telegram import InlineKeyboardMarkup
 from telegram import Update
 from telegram import ParseMode
 from telegram.ext import CallbackContext
-from telegram.ext import CallbackQueryHandler
 from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
-from telegram.ext import Filters
-from telegram.error import TelegramError
+
 
 from bot import MyCommand
 from bot.database import localdatabase as db
-
 
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s',
     level=logging.INFO,
 )
-
 logger = logging.getLogger(__name__)
-
 
 
 def send_booknames(update: Update, context: CallbackContext) -> None:
@@ -37,6 +28,5 @@ def send_booknames(update: Update, context: CallbackContext) -> None:
         for bookname in db_booknames
     ])
     update.message.reply_text(text, parse_mode=ParseMode.MARKDOWN)
-
 
 bookname_handler = CommandHandler(MyCommand.BOOKNAMES, send_booknames)
