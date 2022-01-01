b0dd3e13fc7fbef270e21494e12619594098c303
vbastianpc <vbastianpalavecino@gmail.com>
2022-01-01 20:30:53 -0300

    Init config

--- run_config.py
+++ run_config.py
@@ -1,5 +1,9 @@
+from telegram import Bot
+
+from bot import ADMIN, TOKEN
 from bot.booknames.booknames import add_booknames
-from bot.database import report as db
+from bot.database import report
+from bot.database import localdatabase as db
 from bot.database.schemedb import Language
 from bot.database import SESSION
 from bot.jw.jwlanguage import JWLanguage
@@ -26,12 +30,24 @@ def add_all_languages():
             SESSION.commit()
         except:
             SESSION.rollback()
-        print(i)
         i += 1
-    print(f'There are {db.count_languages()} languages stored in the database')
+    print(f'There are {report.count_languages()} languages stored in the database')
 
 
 if __name__ == '__main__':
+    bot = Bot(TOKEN)
+    msg = bot.send_message(chat_id=ADMIN, text='Preparing configuration...')
     add_all_languages()
-    print('Estoy en run_config.py', __name__)
-    list(map(add_booknames, ['vi', 'es', 'en', 'fr', 'it']))
+    for lang_locale in ['en', 'es', 'vi']:
+        add_booknames(lang_locale)
+    msg = msg.edit_text(f'{msg.text}\nThere are {report.count_languages()} languages stored in the database')
+    print(f'{msg.text!r}')
+    member = bot.get_chat_member(chat_id=ADMIN, user_id=ADMIN)
+    db.set_user(
+        ADMIN,
+        full_name=member.user.full_name,
+        bot_lang=member.user.language_code,
+        brother=True
+    )
+    msg.edit_text(f'{msg.text}\nUser {member.user.full_name} added to database.\nConfiguration succesful!')
+    
