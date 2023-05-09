8d08a7f1839101f9a058d65459dd18c9a4570ae0
vbastianpc <vbastianpalavecino@gmail.com>
2023-05-09 14:59:29 -0400

    add custom __repr__ base declarative

--- bot/database/schema.py
+++ bot/database/schema.py
@@ -3,22 +3,35 @@ https://dbdiagram.io/d/61417a16825b5b0146029d49
 """
 from datetime import datetime, timedelta
 
-from bot.utils import represent as rep
-
-from sqlalchemy import Column, ForeignKey, UniqueConstraint
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.sqltypes import Boolean, Integer, String, DateTime
+from sqlalchemy.sql.sqltypes import Boolean
+from sqlalchemy.sql.sqltypes import Integer
+from sqlalchemy.sql.sqltypes import String
+from sqlalchemy.sql.sqltypes import DateTime
 
 
-Base = declarative_base()
+class Base:
+    """https://stackoverflow.com/a/55749579
+    https://stackoverflow.com/a/54034230"""
+
+    def __repr__(self):
+        params = [f'{k}={v!r}' for k, v in self.__dict__.items() if k not in ['_sa_instance_state', '_sa_adapter']]
+        params = ',\n    '.join(params)
+        return f'<{self.__class__.__name__}(\n    {params}\n)>'
+
+
+Base = declarative_base(cls=Base)
 
 
 class Language(Base):
     __tablename__ = 'Language'
     id = Column('LanguageId', Integer, primary_key=True)
-    meps_symbol = Column('LanguageMepsSymbol', String, unique=True, nullable=False) #, sqlite_on_conflict_unique='IGNORE')
-    code = Column('LanguageCode', String, unique=True, nullable=False) #, sqlite_on_conflict_unique='IGNORE')
+    meps_symbol = Column('LanguageMepsSymbol', String, unique=True, nullable=False)
+    code = Column('LanguageCode', String, unique=True, nullable=False)
     name = Column('LanguageName', String)
     vernacular = Column('LanguageVernacular', String)
     rsconf = Column('RsConfigSymbol', String)
@@ -27,15 +40,16 @@ class Language(Base):
     script = Column('LanguageScript', String)
     is_rtl = Column('IsRTL', Boolean)
 
-    bible = relationship('Bible', back_populates='language', foreign_keys='[Bible.language_id]')
-    sign_language_users = relationship('User', back_populates='sign_language', foreign_keys='[User.sign_language_id]')
-    bot_language_users = relationship('User', back_populates='bot_language', foreign_keys='[User.bot_language_id]')
-    overlay_users = relationship('User', back_populates='overlay_language', foreign_keys='[User.overlay_language_id]')
-    overlay_verses = relationship('File', back_populates='overlay_language', foreign_keys='[File.overlay_language_id]')
+    bible = relationship('Bible', back_populates='language', foreign_keys='[Bible.language_id]')  # type: Bible
+    sign_language_users = relationship('User', back_populates='sign_language',
+                                       foreign_keys='[User.sign_language_id]')  # type: User
+    bot_language_users = relationship('User', back_populates='bot_language',
+                                      foreign_keys='[User.bot_language_id]')  # type: User
+    overlay_users = relationship('User', back_populates='overlay_language',
+                                 foreign_keys='[User.overlay_language_id]')  # type: User
+    overlay_verses = relationship('File', back_populates='overlay_language',
+                                  foreign_keys='[File.overlay_language_id]')  # type: File
 
-    def __repr__(self):
-        return f"<Language(code={self.code!r}, code={self.code!r}, name={self.name!r}, "\
-            f"vernacular={self.vernacular!r}, rsconf={self.rsconf!r}, lib={self.lib!r})>"
 
 class Bible(Base):
     __tablename__ = 'Bible'
@@ -46,8 +60,8 @@ class Bible(Base):
     symbol = Column('SymbolEdition', String)
     url = Column('URL', String)
 
-    language = relationship('Language', back_populates='bible', foreign_keys=[language_id])
-    books = relationship('Book', back_populates='bible', foreign_keys='[Book.bible_id]')
+    language = relationship('Language', back_populates='bible', foreign_keys=[language_id])  # type: Language
+    books = relationship('Book', back_populates='bible', foreign_keys='[Book.bible_id]')  # type: Book
 
 
 class Book(Base):
@@ -74,8 +88,8 @@ class Book(Base):
     book_display_title = Column('BookDisplayTitle', String)
     chapter_display_title = Column('ChapterDisplayTitle', String)
 
-    bible = relationship('Bible', back_populates='books', foreign_keys=[bible_id])
-    chapters = relationship('Chapter', back_populates='book', foreign_keys='[Chapter.book_id]')
+    bible = relationship('Bible', back_populates='books', foreign_keys=[bible_id])  # type: Bible
+    chapters = relationship('Chapter', back_populates='book', foreign_keys='[Chapter.book_id]')  # type: Chapter
     # files = relationship(
     #     'File',
     #     cascade="all,delete",
@@ -83,9 +97,6 @@ class Book(Base):
     #     passive_deletes=True,
     #     foreign_keys='[File.bible_book_id]'
     # )
-
-    def __repr__(self):
-        return f"<BibleBook(number={self.number!r}, bookname={self.name!r})>"
 
 
 class Chapter(Base):
@@ -100,18 +111,15 @@ class Chapter(Base):
     url = Column('URL', String)
     title = Column('Title', String)
 
-    book = relationship('Book', back_populates='chapters', foreign_keys=[book_id])
-    files = relationship('File', back_populates='chapter', foreign_keys='[File.chapter_id]')
+    book = relationship('Book', back_populates='chapters', foreign_keys=[book_id])  # type: Book
+    files = relationship('File', back_populates='chapter', foreign_keys='[File.chapter_id]')  # type: File
     video_markers = relationship(
         'VideoMarker',
         # cascade="all,delete",
         back_populates='chapter',
         foreign_keys='[VideoMarker.chapter_id]'
         # passive_deletes=True,
-    )
-
-    def __repr__(self):
-        return f"<Chapter(number={self.number!r}, checksum={self.checksum!r})>"    
+    )  # type: VideoMarker
 
 
 class VideoMarker(Base):
@@ -125,13 +133,10 @@ class VideoMarker(Base):
     start_time = Column('StartTime', String)
     end_transition_duration = Column('EndTransitionDuration', String)
 
-    chapter = relationship('Chapter', back_populates='video_markers', foreign_keys=[chapter_id])
+    chapter = relationship('Chapter', back_populates='video_markers', foreign_keys=[chapter_id])  # type: Chapter
 
-    def __repr__(self):
-        return f"<VideoMarker(label={self.label!r}, versenum={self.versenum!r}, start_time={self.start_time!r}, " \
-            f"duration={self.duration!r}, end_transition_duration={self.end_transition_duration!r})>"
 
-class File(Base): # old File
+class File(Base):  # old File
     __tablename__ = 'File'
 
     id = Column('FileId', Integer, primary_key=True)
@@ -139,29 +144,24 @@ class File(Base): # old File
     telegram_file_id = Column('TelegramFileId', String, unique=True)
     telegram_file_unique_id = Column('TelegramFileUniqueId', String, unique=True)
     duration = Column('Duration', Integer)
-    name = Column('FileName', String) # Path.stem it must be in written sign language. "Mateo 24:14 - SCH"
+    name = Column('FileName', String)
     raw_verses = Column('RawVerseNumbers', String)
     is_single_verse = Column('IsSingleVerse', Boolean)
     size = Column('FileSize', Integer)
     added_datetime = Column('AddedDatetime', DateTime)
     overlay_language_id = Column('OverlayLanguageId', Integer, ForeignKey('Language.LanguageId'))
 
-    chapter = relationship('Chapter', back_populates='files', foreign_keys=[chapter_id]) # type: Chapter
-    overlay_language = relationship('Language', back_populates='overlay_verses', foreign_keys=[overlay_language_id])
-    
-    users = relationship('User', back_populates='files', secondary='File2User')
+    chapter = relationship('Chapter', back_populates='files', foreign_keys=[chapter_id])  # type: Chapter
+    overlay_language = relationship('Language', back_populates='overlay_verses',
+                                    foreign_keys=[overlay_language_id])  # type: Language
+    users = relationship('User', back_populates='files', secondary='File2User')  # type: User
 
     def get_language(self) -> Language:
         return self.chapter.book.bible.language
-    
+
     def get_book(self) -> Book:
         return self.chapter.book
 
-    def __repr__(self):
-        """This is best https://stackoverflow.com/a/55749579. Change later"""
-        return rep(self, id=self.id, chapter_id=self.chapter_id, telegram_file_id=self.telegram_file_id,
-            telegram_file_unique_id=self.telegram_file_unique_id, duration=self.duration, name=self.name, raw_verses=self.raw_verses,
-            size=self.size, added_datetime=self.added_datetime, overlay_language_id=self.overlay_language_id)
 
 class User(Base):
     __tablename__ = 'User'
@@ -170,7 +170,7 @@ class User(Base):
     DENIED = -1
 
     id = Column('UserId', Integer, primary_key=True)
-    telegram_user_id = Column('TelegramUserId', Integer, unique=True)
+    telegram_user_id = Column('TelegramUserId', Integer, unique=True, nullable=False)
     full_name = Column('FullName', String)
     sign_language_id = Column('SignLanguageId', Integer, ForeignKey('Language.LanguageId'))
     bot_language_id = Column('BotLanguageId', Integer, ForeignKey('Language.LanguageId'))
@@ -178,30 +178,28 @@ class User(Base):
     status = Column('Status', Integer)
     added_datetime = Column('AddedDatetime', DateTime)
     last_active_datetime = Column('LastActiveDatetime', DateTime)
-    
-    bot_language = relationship('Language', back_populates='bot_language_users', foreign_keys=[bot_language_id]) # type: Language
-    sign_language = relationship('Language', back_populates='sign_language_users', foreign_keys=[sign_language_id]) # type: Language
-    overlay_language = relationship('Language', back_populates='overlay_users', foreign_keys=[overlay_language_id]) # type: Language
-    files = relationship('File', back_populates='users', secondary='File2User') # type: File
 
-    def __repr__(self):
-        return f"<User(id={self.id!r}, telegram_user_id={self.telegram_user_id!r}, status={self.status!r}, " \
-            f"sign_language_id={self.sign_language_id!r}, full_name={self.full_name!r}," \
-            f"added_datetime={self.added_datetime!r}, overlay_language_id={self.overlay_language_id})>"
+    bot_language = relationship('Language', back_populates='bot_language_users',
+                                foreign_keys=[bot_language_id])  # type: Language
+    sign_language = relationship('Language', back_populates='sign_language_users',
+                                 foreign_keys=[sign_language_id])  # type: Language
+    overlay_language = relationship('Language', back_populates='overlay_users',
+                                    foreign_keys=[overlay_language_id])  # type: Language
+    files = relationship('File', back_populates='users', secondary='File2User')  # type: File
 
     def is_authorized(self) -> bool:
         return self.status == self.AUTHORIZED
-    
+
     def is_waiting(self) -> bool:
         return self.status == self.WAITING
-    
+
     def is_denied(self) -> bool:
         return self.status == self.DENIED
-    
+
     def is_active(self) -> bool:
         _30_days_ago = datetime.now() - timedelta(days=30)
         return self.last_active_datetime > _30_days_ago
-    
+
 
 class File2User(Base):
     __tablename__ = 'File2User'
@@ -210,7 +208,3 @@ class File2User(Base):
     file_id = Column('FileId', Integer, ForeignKey('File.FileId'), nullable=False)
     user_id = Column('UserId', Integer, ForeignKey('User.UserId'), nullable=False)
     datetime = Column('Datetime', DateTime)
-
-    def __repr__(self):
-        return f"<UserRequest(file_id={self.file_id!r}, user_id={self.user_id!r}, " \
-            f"datetime={self.datetime!r})>"
