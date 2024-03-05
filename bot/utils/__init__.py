4304c73808a9f80b468488899a9f74c3967d1dfc
vbastianpc <vbastianpalavecino@gmail.com>
2024-03-05 19:18:52 -0300

    remove now function

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,6 +1,5 @@
 from bot.utils.utils import list_of_lists
 from bot.utils.utils import safechars
-from bot.utils.utils import now
 from bot.utils.utils import dt_now
 from bot.utils.utils import how_to_say
 from bot.utils.utils import strike
